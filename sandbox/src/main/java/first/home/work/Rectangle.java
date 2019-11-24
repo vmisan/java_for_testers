7d6212e862d95a75c3a6a255c914c454486c4838
vmisan <vimi@relesys.net>
2019-11-24 16:01:56 +0300

    Добавил классы Square и Rectangle + конструкторы обьектов

---
+++ sandbox/src/main/java/first/home/work/Rectangle.java
@@ -0,0 +1,11 @@
+package first.home.work;
+
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b){
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/first/home/work/Square.java
@@ -0,0 +1,9 @@
+package first.home.work;
+
+public class Square {
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+}
