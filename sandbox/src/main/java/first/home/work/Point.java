01ff9efe63dfa354e1be1c7eedb1034ec3201a99
vmisan <vimi@relesys.net>
2019-11-27 01:42:28 +0300

    1.2.1. Реализовал метод, через который идет расчет расстояния между точками

--- sandbox/src/main/java/first/home/work/Point.java
+++ sandbox/src/main/java/first/home/work/Point.java
@@ -1,5 +1,7 @@
 package first.home.work;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 public class Point {
   public double x;
   public double y;
@@ -7,6 +9,17 @@ public class Point {
   public Point(double x, double y) {
     this.x = x;
     this.y = y;
+  }
+
+  public double distance(Point pointToCalculate){
+    double a = this.x - pointToCalculate.x;
+    double b = this.y - pointToCalculate.y;
+    double c = 2;
+    return Math.sqrt(Math.pow(a,c) + Math.pow(b,c));
 
   }
+
 }
+
+
+
