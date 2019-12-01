396fd80e10696384f6f3127264fb4482cc517d2b
vmisan <vimi@relesys.net>
2019-12-01 14:48:07 +0300

    Update DistanceTest.java

--- sandbox/src/test/java/first/home/work/DistanceTest.java
+++ sandbox/src/test/java/first/home/work/DistanceTest.java
@@ -7,7 +7,7 @@ import org.testng.asserts.Assertion;
 public class DistanceTest {
 
   @Test
-          public void testDistance(){
+  public void testDistance(){
     Point p1 = new Point(12, 18);
     Point p2 = new Point(19, 25);
     Assert.assertNotEquals(p1.distance(p2), 0);
@@ -18,6 +18,27 @@ public class DistanceTest {
     Point p1 = new Point(12, 18);
     Point p2 = new Point(19, 25);
     Assert.assertNotNull(p1.distance(p2), "Distance between points should not be NULL");
+  }
+
+  @Test
+  public void testDistance3(){
+    Point p1 = new Point(-1, 0);
+    Point p2 = new Point(0, 1);
+    Assert.assertEquals(p1.distance(p2), 1.4142135623730951);
+  }
+
+  @Test
+  public void testDistance4(){
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 0);
+    Assert.assertEquals(p1.distance(p2), 0);
+  }
+
+  @Test
+  public void testDistance5(){
+    Point p1 = new Point(1, -1);
+    Point p2 = new Point(-1, 1);
+    Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
   }
 
 }
