b88eed65b56e76ef30d515e79e05e05ffa43214e
vmisan <vimi@relesys.net>
2019-12-15 23:34:02 +0300

    Задание 7. Добавил оператор equals для запусков тестов в различных браузерах

---
+++ sandbox/src/main/java/first/home/work/Equality.java
@@ -0,0 +1,12 @@
+package first.home.work;
+
+public class Equality {
+
+  public static void main(String[] args){
+    String s1 = "firefox";
+    String s2 = new String(s1);
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+  }
+}
